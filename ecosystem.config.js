1ad60bac40bac898193fa6de5b9608ad82e57db9
Karel Puerto Diaz <karelpuerto78@gmail.com>
2025-08-08 14:54:51 -0600

    fix port 3000

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,11 +11,11 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'development',
-        PORT: 80
+        PORT: 3000
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 80
+        PORT: 3000
       }
     }
   ]
