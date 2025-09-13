cdc4b3ab9656a9819aa46a896f308143a8a07f96
Miguel Luque <miguel.luque@samtel.co>
2025-09-12 19:08:30 -0500

    Ajustes build

---
+++ src/components/ui/toast.tsx
@@ -0,0 +1,13 @@
+
+export type ToastActionElement = React.ReactNode;
+
+export interface ToastProps {
+  id: string;
+  title?: string;
+  description?: string;
+  action?: ToastActionElement;
+  duration?: number;
+  onClose?: () => void;
+}
+
+
